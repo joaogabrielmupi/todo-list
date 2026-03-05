974e6c944d0fcff7e62a580c3ffa389018a8d569
Mupi Systems <joao.brito@mupisystems.com.br>
2026-03-05 13:41:07 -0300

    Frontend

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create', views.create_task, name='create-task'),
-    path('delete/<int:task_id>', views.delete_task, name='delete-task'),
-    path('update/<int:task_id>', views.update_task_status, name='update-task'),
+    path('create/', views.create_task, name='create-task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete-task'),
+    path('update/<int:pk>/', views.update_task_status, name='update-task'),
+    path('edit/<int:pk>/', views.edit_task, name='edit-task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -20,7 +20,18 @@ def create_task(request):
         return HttpResponse('Dados inválidos', status=400)
 
 
-    return HttpResponse(status=400)
+    return render(request, 'partials/task_modal_content.html', {'modal_title' : 'Nova Tarefa'})
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            return render(request, 'partials/task_item.html', {'task' : task})
+        return HttpResponse("Erro ao atualizar", status=400)
+
+    return render(request, 'partials/task_modal_content.html', {'task' : task, 'modal_title': 'Editar Tarefa'})
 
 def delete_task(request, pk):
     task = get_object_or_404(Task, pk=pk)
